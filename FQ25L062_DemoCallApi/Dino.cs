1c002ef1edfff0b09dd90944d24a86292fa56697
Thierry Morre <thierry.morre@cognitic.be>
2025-09-26 10:16:29 +0200

    Ajoutez des fichiers projet.

---
+++ FQ25L062_DemoCallApi/Dino.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FQ25L062_DemoCallApi
+{
+    internal class Dino
+    {
+        public int Id { get; }
+        public string Espece { get; }
+        public int Poids { get; set; }
+        public int Taille { get; set; }
+
+        public Dino(int id, string espece, int poids, int taille)
+        {
+            Id = id;
+            Espece = espece;
+            Poids = poids;
+            Taille = taille;
+        }
+    }
+}
---
+++ FQ25L062_DemoCallApi/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using FQ25L062_DemoCallApi;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+//Call GET & DELETE ne peuvent pas envoyer de contenu
+// Exemple de Get
+using (HttpClient client = new HttpClient())
+{
+    //using (HttpResponseMessage responseMessage = client.GetAsync("https://localhost:7093/api/Dino").Result)
+    //{
+    //    //responseMessage.EnsureSuccessStatusCode(); //Si Code 20x pas de soucis sinon Exception
+
+    //    if(responseMessage.IsSuccessStatusCode)
+    //    {
+    //        string json = responseMessage.Content.ReadAsStringAsync().Result;
+    //        Console.WriteLine(json);
+    //        IEnumerable<Dino>? dinos = JsonSerializer.Deserialize<Dino[]>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+    //        if(dinos is not null)
+    //        {
+    //            foreach(Dino dino in dinos)
+    //            {
+    //                Console.WriteLine(dino.Espece ?? "Null");
+    //            }
+    //        }
+    //    }
+    //}
+
+    // Méthode simplifiée
+    //try
+    //{
+    //    IEnumerable<Dino>? dinos = client.GetFromJsonAsync<Dino[]>("https://localhost:7093/api/Dino").Result;
+
+    //    if (dinos is not null)
+    //    {
+    //        foreach (Dino dino in dinos)
+    //        {
+    //            Console.WriteLine(dino.Espece ?? "Null");
+    //        }
+    //    }
+    //}
+    //catch (Exception ex)
+    //{
+    //    Console.WriteLine(ex.Message);
+    //}
+
+    int id = 1;
+    using (HttpResponseMessage responseMessage = client.GetAsync($"https://localhost:7093/api/Dino/{id}").Result)
+    {
+
+        //responseMessage.EnsureSuccessStatusCode(); //Si Code 20x pas de soucis sinon Exception
+
+        if (!responseMessage.IsSuccessStatusCode)
+        {
+            switch(responseMessage.StatusCode)
+            {
+                case HttpStatusCode.BadRequest:
+                    Console.WriteLine("Il y a une erreur dans la requête!");
+                    break;
+                case HttpStatusCode.NotFound:
+                    Console.WriteLine($"Aucun dinosaure trouvé avec l'id => {id}");
+                    break;
+            }
+
+        }
+        else
+        {
+            string json = responseMessage.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(json);
+            Dino? dino = JsonSerializer.Deserialize<Dino>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            if (dino is not null)
+            {
+                Console.WriteLine(dino.Espece);
+            }
+        }
+    }
+
+    //try
+    //{
+    //    Dino? dino = client.GetFromJsonAsync<Dino>($"https://localhost:7093/api/Dino/{id}").Result;
+
+    //    if (dino is not null)
+    //    {
+    //        Console.WriteLine(dino.Espece);
+    //    }
+    //}
+    //catch (AggregateException ex)
+    //{
+    //    Console.WriteLine(ex.Message);
+    //}
+}
+
+//Call POST, PUT, PATCH un contenu sera attendu
